654aeaeb2cce39e4ba70fbd71586f85737dc6325
tage <896932646@qq.com>
2016-06-25 17:03:45 +0800

    测试node-canvas安装

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var Canvas = require('canvas');
+
+
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -8,8 +12,24 @@ router.get('/', function (req, res, next) {
 
 router.get(/\/([\d]+)[x|X]([\d]+)/, (req, res, next)=> {
     console.log(req.params);
-    let {'0':_width,'1':_height}=req.params;
+    let {'0':_width, '1':_height}=req.params;
+
+    let canvas = new Canvas(200, 200)
+        , ctx = canvas.getContext('2d');
+
+    ctx.font = '30px Impact';
+    ctx.rotate(.1);
+    ctx.fillText("Awesome!", 50, 100);
+    var te = ctx.measureText('youudehexie!');
+    ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+    ctx.beginPath();
+    ctx.lineTo(50, 102);
+    ctx.lineTo(50 + te.width, 102);
+    ctx.stroke();
+
+    console.log('<img src="' + canvas.toDataURL() + '" />');
 
 });
 
 module.exports = router;
+
