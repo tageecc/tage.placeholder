e59b6a0a6b1a54cea34c232fd793822971a9f387
tage <896932646@qq.com>
2016-06-25 16:23:21 +0800

    添加placeholder url解析，gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+router.get(/\/([\d]+)[x|X]([\d]+)/, (req, res, next)=> {
+    console.log(req.params);
+    let {'0':_width,'1':_height}=req.params;
+
 });
 
 module.exports = router;
