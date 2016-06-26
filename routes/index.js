3fcf458fccf4994cde2df7661f6314eec445d986
tage <896932646@qq.com>
2016-06-26 14:45:38 +0800

    添加自定义rgb颜色功能

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,19 @@
-var express = require('express');
-var router = express.Router();
-var images = require("images");
+let express = require('express');
+let router = express.Router();
+let images = require('images');
+let Color = require('../util/color');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+    res.render('index', {title: 'placeholder-images'});
 });
 
 router.get(/\/([\d]+)[x|X]([\d]+)/, (req, res, next)=> {
     let {'0':_width, '1':_height}=req.params;
+    let _rgba=Color.parseRGB(req.query.color);
+    let [_color=[204,204,204]]=[[_rgba[1],_rgba[2],_rgba[3]]];
     let img = images(parseInt(_width), parseInt(_height))
-        .fill(204,204,204,1)
+        .fill(_color[0],_color[1],_color[2])
         .encode("jpg");
     res.contentType('image/jpeg');
     res.write(img,'binary');
---
+++ util/color.js
@@ -0,0 +1,58 @@
+let reg = /^#([0-9a-fA-f]{3}|[0-9a-fA-f]{6})$/;
+
+exports.parseHex = function (str) {
+    if (/^(rgb|RGB)/.test()) {
+        var aColor = str.replace(/(?:||rgb|RGB)*/g, "").split(",");
+        var strHex = "#";
+        for (let i = 0; i < aColor.length; i++) {
+            var hex = Number(aColor[i]).toString(16);
+            if (hex === "0") {
+                hex += hex;
+            }
+            strHex += hex;
+        }
+        if (strHex.length !== 7) {
+            strHex = str;
+        }
+        return strHex;
+    } else if (reg.test(str)) {
+        var aNum = str.replace(/#/, "").split("");
+        if (aNum.length === 6) {
+            return str;
+        } else if (aNum.length === 3) {
+            var numHex = "#";
+            for (let i = 0; i < aNum.length; i += 1) {
+                numHex += (aNum[i] + aNum[i]);
+            }
+            return numHex;
+        }
+    } else {
+        return str;
+    }
+};
+
+exports.parseRgb = function (str) {
+    var sColor = str.toLowerCase();
+    if (sColor && reg.test(sColor)) {
+        if (sColor.length === 4) {
+            let sColorNew = "#";
+            for (let i = 1; i < 4; i += 1) {
+                sColorNew += sColor.slice(i, i + 1).concat(sColor.slice(i, i + 1));
+            }
+            sColor = sColorNew;
+        }
+        //处理六位的颜色值
+        var sColorChange = [];
+        for (let i = 1; i < 7; i += 2) {
+            sColorChange.push(parseInt("0x" + sColor.slice(i, i + 2)));
+        }
+        return "RGB(" + sColorChange.join(",") + ")";
+    } else {
+        return sColor;
+    }
+};
+
+exports.parseRGB = function (str) {
+    let reg_rgba = /[RGB|rgb]\(([\d]+),([\d]+),([\d]+)\)/;
+        return str.match(reg_rgba).map(_str=>parseInt(_str));
+};
