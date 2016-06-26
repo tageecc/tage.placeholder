5e4c2071f170b40adc02ac89d1c7346e3237cf9c
tage <896932646@qq.com>
2016-06-26 17:26:18 +0800

    添加png图片格式，添加png透明度设置功能

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,19 @@ router.get('/', function (req, res, next) {
 
 router.get(/\/([\d]+)[x|X]([\d]+)/, (req, res, next)=> {
     let {'0':_width, '1':_height}=req.params;
-    let _rgba=Color.parseRGB(req.query.color);
-    let [_color=[204,204,204]]=[[_rgba[1],_rgba[2],_rgba[3]]];
+    let _rgba = Color.parseRGB(req.query.color);
+    let [_color=[204, 204, 204],_type='jpg',_opacity=1]=[[_rgba[1], _rgba[2], _rgba[3]], req.query.type, parseFloat(req.query.opacity)];
+    let _type_index = ['jpg', 'png'].findIndex(item=>item == _type);
+    if (_type_index < 0) {
+        res.status(404);
+        res.end();
+        return;
+    }
     let img = images(parseInt(_width), parseInt(_height))
-        .fill(_color[0],_color[1],_color[2])
-        .encode("jpg");
-    res.contentType('image/jpeg');
-    res.write(img,'binary');
+        .fill(_color[0], _color[1], _color[2], _opacity)
+        .encode(_type);
+    res.contentType('image/' + ['jpeg', 'png'][_type_index]);
+    res.write(img, 'binary');
     res.end();
 });
 
